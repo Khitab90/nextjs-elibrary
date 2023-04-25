b12f556817b3a55aadec4a1c6497c854d96e4114
Khitab90 <khitabanand435@gmail.com>
2023-04-25 16:05:33 -0700

    add tailwind dark theem, dynamic routes

--- components/Books.js
+++ components/Books.js
@@ -1,14 +1,19 @@
 import Book from './Book';
 
-const Books = ({ data }) => {
+const Books = ({ data, searchString }) => {
+  const checkCondition = (e) => {
+    return e.toLowerCase().includes(searchString.toLowerCase());
+  }
   return (
     <section className="grid justify-items-center gap-4">
       {data.map((book, index) => {
-        return (
-          <div key={index} className="border border-gray-100 mt-1">
-            <Book book={book} />
-          </div>
-        );
+        if (checkCondition(book.title) || checkCondition(book.author)) {
+          return (
+            <div key={index} className="border border-gray-100 mt-1">
+              <Book book={book} />
+            </div>
+          );
+        }
       })}
     </section>
   );
